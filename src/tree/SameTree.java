90ccac21548048e2bed1d0270dc42de859e50613
benjamin <benjamin.ming.cn@gmail.com>
2019-12-21 17:34:26 +0800

    100. Same Tree

---
+++ src/tree/SameTree.java
@@ -0,0 +1,24 @@
+package tree;
+
+import base.TreeNode;
+
+/**
+ * 100. Same Tree
+ */
+public class SameTree {
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
