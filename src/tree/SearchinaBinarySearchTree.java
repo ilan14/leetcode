2cc0e35206d9bd69e54488369cb24fb31d2418c2
benjamin <benjamin.ming.cn@gmail.com>
2019-12-27 11:33:22 +0800

    700. Search in a Binary Search Tree

---
+++ src/tree/SearchinaBinarySearchTree.java
@@ -0,0 +1,24 @@
+package tree;
+
+import base.TreeNode;
+
+/**
+ * 700. Search in a Binary Search Tree
+ */
+public class SearchinaBinarySearchTree {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == val) {
+            return root;
+        }
+
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        }
+
+        return searchBST(root.right, val);
+    }
+}
