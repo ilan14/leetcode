807ef8d91ff91c116e22dcca9bc8437968fb9413
benjamin <benjamin.ming.cn@gmail.com>
2019-12-27 11:47:48 +0800

    701. Insert into a Binary Search Tree

---
+++ src/tree/InsertintoaBinarySearchTree.java
@@ -0,0 +1,24 @@
+package tree;
+
+import base.TreeNode;
+
+/**
+ * 701. Insert into a Binary Search Tree
+ */
+public class InsertintoaBinarySearchTree {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        if (root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        }
+
+        if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        }
+
+        return root;
+    }
+}
