771317d6ebaf5a837607a7da068536306588cead
Benjamin <benjamin.ming.cn@gmail.com>
2020-09-23 11:37:54 +0800

    138. Copy List with Random Pointer

---
+++ src/linkedlist/CopyListwithRandomPointer.java
@@ -0,0 +1,46 @@
+package linkedlist;
+
+import java.util.HashMap;
+
+/**
+ * 138. Copy List with Random Pointer
+ */
+public class CopyListwithRandomPointer {
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        HashMap<Node, Node> map = new HashMap<>();
+        Node newHead = map.computeIfAbsent(head, k -> new Node(head.val));
+        Node tail = newHead;
+        Node cur = head;
+        while (cur != null) {
+            Node next = cur.next;
+            if (cur.next != null) {
+                tail.next = map.computeIfAbsent(next, k -> new Node(next.val));
+            }
+
+            Node random = cur.random;
+            if (random != null) {
+                tail.random = map.computeIfAbsent(random, k -> new Node(random.val));
+            }
+            cur = cur.next;
+            tail = tail.next;
+        }
+
+        return newHead;
+    }
+}
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
