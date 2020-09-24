c6cfb579300e72c00906dde2cff78c32f2776ca4
Benjamin <benjamin.ming.cn@gmail.com>
2020-09-24 17:44:36 +0800

    141. Linked List Cycle

---
+++ src/linkedlist/LinkedListCycle.java
@@ -0,0 +1,24 @@
+package linkedlist;
+
+import base.ListNode;
+
+/**
+ * 141. Linked List Cycle
+ */
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
