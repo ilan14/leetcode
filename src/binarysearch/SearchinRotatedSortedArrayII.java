58cbb4eeed2cce32d0f6e63a4b52e349576e3b4f
Benjamin <benjamin.ming.cn@gmail.com>
2020-11-17 17:18:15 +0800

    81. Search in Rotated Sorted Array II

---
+++ src/binarysearch/SearchinRotatedSortedArrayII.java
@@ -0,0 +1,36 @@
+package binarysearch;
+
+/**
+ * 81. Search in Rotated Sorted Array II
+ */
+public class SearchinRotatedSortedArrayII {
+    public boolean search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] > nums[high]) {
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else if (nums[mid] < nums[high]) {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            } else {
+                high--;
+            }
+        }
+
+        return false;
+    }
+}
